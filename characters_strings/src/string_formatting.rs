c29dd18c4f2a32fb2f9bcf8d13409b06a4ab1344
csruiliu <rui.liu@gmx.com>
2020-11-10 21:54:46 -0600

    add string format

--- characters_strings/src/string_formatting.rs
+++ characters_strings/src/string_formatting.rs
@@ -1,3 +1,23 @@
+#![allow(dead_code)]
+#![allow(unused_must_use)]
+
 fn main() {
-    println!("Hello, world!");
+    let name = "Rust";
+    let greeting = format!("hi, I'm {}, nice to meet you", name);
+    println!("{}", greeting);
+
+    let hello = "hello";
+    let hello_name = format!("{}, {}!", hello, name);
+    println!("{}", hello_name);
+
+    let run = "run";
+    let forest = "forest";
+    let rfr = format!("{0},{1},{0}", run, forest);
+    println!("{}", rfr);
+
+    let info = format!("the name's {last}. {first} {last}.", first = "james", last = "bond");
+    println!("{}", info);
+
+    let mixed = format!("{1} {} {0} {data}", "alpha", "beta", data="delta");
+    println!("{}", mixed);
 }
