8a76d75b66d7b9018c69c0926d4da727d1104227
csruiliu <rui.liu@gmx.com>
2020-11-18 14:49:00 -0600

    add traits

---
+++ traits/src/traits.rs
@@ -0,0 +1,74 @@
+
+
+trait Animal {
+    fn create(name: &'static str) -> Self;
+
+    fn name(&self) -> &'static str;
+
+    fn talk(&self) {
+        println!("{} cannot talk", self.name());
+    }
+}
+
+struct Human {
+    name: &'static str
+}
+
+struct Cat {
+    name: &'static str
+}
+
+impl Animal for Human {
+    fn create(name: &'static str) -> Self {
+        Human{name: name}
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+    fn talk(&self) {
+        println!("{} says hello", self.name());
+    }
+}
+
+impl Animal for Cat {
+    fn create(name: &'static str) -> Self {
+        Cat{name: name}
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+    fn talk(&self) {
+        println!("{} says meow", self.name());
+    }
+}
+
+trait Summable<T> {
+    fn sum(&self) -> T;
+}
+
+impl Summable<i32> for Vec<i32> {
+    fn sum(&self) -> i32 {
+        let mut result:i32 = 0;
+        for x in self {
+            result += *x;
+        }
+        return result;
+    }
+}
+
+fn traits() {
+    //let h = Human{name:"Human"};
+    let h:Human = Animal::create("Human");
+    h.talk();
+    
+    let c = Cat{name:"Cat"};
+    c.talk();
+
+    let a = vec![1,2,3];
+    println!("sum= {}", a.sum());
+}
+fn main() {
+    traits();
+}
